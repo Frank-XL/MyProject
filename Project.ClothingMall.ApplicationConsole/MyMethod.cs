7dae8dd8dde441bcb773b8962d3f8b5102593768
Frank-XL <ntes_xl@163.com>
2018-05-09 20:17:24 +0800

    添加了注释

--- Project.ClothingMall.ApplicationConsole/MyMethod.cs
+++ Project.ClothingMall.ApplicationConsole/MyMethod.cs
@@ -8,6 +8,7 @@ namespace Project.ClothingMall.ApplicationConsole
 {
     public static class MyMethod
     {
+        //一个简单的斐波那契额方法
         public static int Fib(int n)
         {
             if (n == 1 || n == 2)
