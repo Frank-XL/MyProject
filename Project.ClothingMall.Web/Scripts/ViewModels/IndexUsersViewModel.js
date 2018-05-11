987e00659d6459ebf38899192bd77b5d2d26057a
Frank-XL <ntes_xl@163.com>
2018-05-11 10:01:39 +0800

    Js中调试的代码删除了

--- Project.ClothingMall.Web/Scripts/ViewModels/IndexUsersViewModel.js
+++ Project.ClothingMall.Web/Scripts/ViewModels/IndexUsersViewModel.js
@@ -3,12 +3,8 @@
     self.users = users;
 
     self.showDeleteModal = function (data,event) {
-        
-        alert(event.target.nodeName);
-        alert(data);
         self.sending = ko.observable(false);
         $.get($(event.target).attr('href'), function (d) {
-            alert($('.body-content').html());
             $('.body-content').prepend(d);
             $('#deleteModal').modal('show');
             ko.applyBindings(self, document.getElementById('deleteModal'));
